fd5a3710fc6763198a6da62948e664cb3fbeeb70
kaickzl <kaicksilva74880@gmail.com>
2024-10-14 10:54:15 -0300

    Add files via upload

---
+++ FLASHCARD/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'pessoal',
+    'Qual a minha comida favorita?',
+    'pizza'
+)
+
+criaCartao(
+    'pessoal',
+    'qual Desenho favorito?',
+    'Os Simpsons'
+)
+
+criaCartao(
+    'recomendação',
+    'Musica favortia?',
+    'O fogo nunca dorme, o fogo nunca apaga'
+)
+
+criaCartao(
+    'recomendacão',
+    'jogador preferido?',
+    'Cristiano Ronaldo'
+)
